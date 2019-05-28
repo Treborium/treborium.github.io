00c717daa243a527a447f090faec310bc97af233
Robert Fuchs <RobertFuchs97@gmail.com>
2019-05-28 09:29:17 +0200

    Dynamically add cards by cloning a template

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function toggleMenu(x) {
     x.classList.toggle("change");
-} 
+}
+
+window.addEventListener("DOMContentLoaded", function() {
+    var card = document.getElementById("card-template");
+
+    for (var i = 0; i < 10; i++) {
+        document.getElementById("posts").appendChild(card.cloneNode(true));
+    }
+}, false);
