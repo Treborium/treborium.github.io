dd922b4541dd35bbb30b24d4dd921500bd6dcf29
Robert Fuchs <RobertFuchs97@gmail.com>
2019-05-28 10:36:01 +0200

    Load card templates dynamically
    Card templates are now defined in the javascript file with the help of lit-html.

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
+import { html, render } from 'https://unpkg.com/lit-html?module';
+
 function toggleMenu(x) {
     x.classList.toggle("change");
 }
 
 window.addEventListener("DOMContentLoaded", function() {
-    var card = document.getElementById("card-template");
+    const cardTemplate = getCardTemplate();
+    const cardContainerTemplate = getCardContainerTemplate();
 
-    for (var i = 0; i < 10; i++) {
-        document.getElementById("posts").appendChild(card.cloneNode(true));
-    }
+    let data = {
+        title: "My Amazing Title!",
+        tags: "Python",
+        date: "21. May 2019"
+    };
+
+    let data2 = {
+        title: "Another Title!",
+        tags: "javascript",
+        date: "24. May 2019"
+    };
+
+    const cards = [];
+    cards.push(cardTemplate(data))
+    cards.push(cardTemplate(data2))
+
+
+    posts = document.getElementById("posts")
+    render(cardContainerTemplate(cards), posts);
 }, false);
+
+function getCardTemplate() {
+    return (data) => html `
+        <div class="card clickable" id="card-template">
+            <div class="post-left">
+                <img class="thumbnail" src="img/dummy.png" alt="thumbnail" />
+            </div>
+            <div class="post-right">
+                <h2 class="post-title">${data.title}</h2>
+                <div class="post-right-bottom">
+                    <h3 class="post-meta-info">${data.tags} - ${data.date}</h3>
+                </div>
+            </div>
+        </div>
+    `;
+}
+
+function getCardContainerTemplate() {
+    return (cards) => html `
+        <div class="blog-posts">
+            ${cards.map((card) => html`${card}`)}
+        </div>`;
+}
